8d5d0035605edadd031d1fe0969a3c4856406e69
Jarno Tuovinen <spedepekka@gmail.com>
2016-03-05 18:52:07 +0200

    Able to run many games

--- run-all.sh
+++ run-all.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
+#
+# Play hunter vs mastermind many times.
+#
+# USAGE: <SCRIPT> <GAMES>
+#
 
+GAMES="${1}"
+if [ -z "${GAMES}" ]; then
+  GAMES=1
+fi
+
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly GAMELOG="games.log"
+readonly GAMEERR="games.err"
+
+if [ -f "${GAMELOG}" ]; then
+  rm "${GAMELOG}"
+fi
+if [ -f "${GAMEERR}" ]; then
+  rm "${GAMEERR}"
+fi
+
+# Activate virtualenv for Python client
 activate() {
   source venv/bin/activate
 }
 
-readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "${DIR}"
-pushd server
-./run-server.sh > run-server.log 2>run-server.err &
-popd
-pushd ./clients/javascript
-./mastermind.sh 2>&1 > /dev/null &
-popd
-pushd ./clients/python
-activate
-./hunter.sh
-popd
-popd
+play() {
+  pushd "${DIR}"
+  pushd server
+  ./run-server.sh > run-server.log 2>run-server.err &
+  sleep 1
+  popd
+  pushd ./clients/javascript
+  ./mastermind.sh 2>&1 > /dev/null &
+  sleep 1
+  popd
+  pushd ./clients/python
+  activate
+  ./hunter.sh >> "${DIR}/${GAMELOG}" 2>> "${DIR}/${GAMEERR}"
+  popd
+  popd
+}
+
+COUNTER=1
+while [  ${COUNTER} -le ${GAMES} ]; do
+  echo "GAME ${COUNTER}"
+  play
+  let COUNTER=COUNTER+1 
+done
+
+WON=$(grep "Game ended. You win" "${GAMEERR}" | wc -l)
+PERCENT=$(bc <<< "scale=2; ${WON}/${GAMES}*100")
+
+echo "WON: ${WON} / ${GAMES}, ${PERCENT}"
